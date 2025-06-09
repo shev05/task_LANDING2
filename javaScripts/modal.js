7407a0776e26b419ae8d516d9068fdbcc2fb4cdd
Shevchnko Evgeniy <zhenshev05@mail.ru>
2025-06-09 17:10:40 +0300

    feat/add-footer-and-modal

---
+++ javaScripts/modal.js
@@ -0,0 +1,16 @@
+function openModal() {
+    event.preventDefault(); 
+    document.getElementById('modal').style.display = 'flex';
+    
+}
+
+function closeModal() {
+    document.getElementById('modal').style.display = 'none';
+}
+
+window.onclick = function(event) {
+    const modal = document.getElementById('modal');
+    if (event.target === modal) {
+        closeModal();
+    }
+}
